efeaebce86775d4883ec770c833c50b2e872246f
JuBragada <juliabragada@gmail.com>
2024-06-13 22:29:18 -0300

    teste

---
+++ aula.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("DESAFIO DO CAÍQUE")
+print(145+234)
+nome = "Julia"
+print('Meu nome é',nome)
+soma = 235 + 125
+print(soma)
+"""
+Este é um comentário, usado para fazer com várias linhas
+"""
+# Este também é um comentário, usado para uma linha simples

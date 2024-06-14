1cc6372a2a968869632b4ab20c53f166f9694875
JuBragada <juliabragada@gmail.com>
2024-06-13 22:30:54 -0300

    novos arquivos

---
+++ Input.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input(" Digite sua altura: "))
+print(type(idade))
+
+print(f"O nome da pessoa é {nome}, e a idade é {idade}, e a idade formatada é {idade_formatada}")
---
+++ aula1.py
@@ -0,0 +1,14 @@
+"""
+Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.
+"""
+nome_completo = "PAULA MARTINS"
+nome = "PAULA"
+mês = "JANEIRO"
+valor = "R$ 500,00"
+desconto = 10
+cupom = nome+"É"+str(desconto)
+
+
+f"O nome da pessoa é {nome}"
+tipo_nome = type(nome)
+print(tipo_nome)
